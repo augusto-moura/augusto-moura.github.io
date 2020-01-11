a89eef5d01ed514a712b85ff90b4a2eee7391c70
Augusto Moura <augusto_moura40@hotmail.com>
2020-01-11 01:12:24 -0300

    Speedrun - MMLB + Srcom

--- src/components/pages/Speedrun.vue
+++ src/components/pages/Speedrun.vue
@@ -1,38 +1,98 @@
 <template>
 	<div>
+		<h1 class="text-primary">Speedrun</h1>
+
 		<p>
-			Em construção.
+			Em videogames, speedrun significa completar um objetivo dentro de um jogo no menor tempo possível. É um desafio que adiciona profundidade e dificuldade aos jogos quando comparados a playthroughs casuais, e bons resultados dependem de muita dedicação e habilidade.
+		</p>
+
+		<p>
+			Speedruns são divididos em categorias que ditam os objetivos que têm que ser alcançados e as limitações, ou seja, o que é proibido fazer. Geralmente os jogos possuem pelo menos uma categoria em que tudo é liberado, chamadas de any%, em que é permitido o uso inclusive de erros na programação, exploits, e tudo que é alcançável sem uso de ferramentas externas como cheat codes. Para os que não gostam de glitches, é muito comum existir uma categoria que não permite o uso de glitches, então tem para todos os gostos .
+		</p>
+
+		<p>
+			Links:
+		</p>
+		<ul>
+			<li>
+				<a href="http://www.speedrun.com/user/Titanish" target="_blank">
+					Meu perfil no Speedrun.com
+				</a>
+			</li>
+			<li>
+				<a href="http://megamanleaderboards.net/index.php?page=runner&name=Titanish" target="_blank">
+					Meu perfil no Mega Man Leaderboards
+				</a>
+			</li>
+			<li>
+				<a href="https://www.youtube.com/watch?v=j1jIY8LxDzs" target="_blank">
+					Guia de speedrun para iniciantes (feito por mim)
+				</a>
+			</li>
+			<li>
+				<a href="http://wiki.speedrunsbrasil.com/Gloss%C3%A1rio" target="_blank">
+					Glossário (termos utilizados) de speedrun
+				</a>
+			</li>
+		</ul>
+
+		<hr>
+
+		<p>
+			Abaixo, meus PBs (personal bests), isto é, meus recordes pessoais em cada jogo que fiz speedrun.
 		</p>
 
 		<div v-if="pbs">
 			<div
-				v-for="pb in pbs"
-				:key="pb.run.id"
+				v-for="(pbsArray, game) in pbs"
+				:key="game"
 			>
-				{{ pb.game.data.names.international }}
-				-
-				{{ pb.category.data.name }}
-				-
-				{{ pb.run.times.primary }}
+				<h2>
+					{{ game }}
+				</h2>
+				<ul v-if="Array.isArray(pbsArray) && pbsArray.length > 0">
+					<li
+						v-for="pb in pbsArray"
+						:key="pb.id"
+					>
+						{{ pb.category }}
+						-
+						{{ pb.time }}
+					</li>
+				</ul>
 			</div>
 		</div>
+		<p v-else>
+			Carregando...
+		</p>
 	</div>
 </template>
 
 <script>
+import {moment, duration} from 'moment'
 export default {
 	computed: {
-		totalPbs: function(){
-			return this.pbs.length;
+		pbs: function(){
+			return [
+				...( Array.isArray(this.srcomPBs) ? this.srcomPBs : [] ),
+				...( Array.isArray(this.mmlbPBs) ? this.mmlbPBs : [] ),
+			]
+			.sort((a, b) => (a.game > b.game) ? 1 : -1) //ordem alfabética
+			.reduce((acc, curr) => { //agrupar por nome de jogo
+				if(!acc[curr.game]) acc[curr.game] = [];
+				acc[curr.game].push(curr);
+				return acc;
+			},{});
 		}
 	},
 	data(){
 		return {
-			pbs: null,
+			srcomPBs: null,
+			mmlbPBs: null
 		}
 	},
 	methods: {
-		searchPbs(){
+		searchSrcomPbs(){
 			fetch(`https://www.speedrun.com/api/v1/users/18vnevjl/personal-bests?embed=game,category`, {
 				headers: new Headers({
 					'User-agent': 'Mozilla/4.0 Custom User Agent'
@@ -40,29 +100,46 @@ export default {
 			})
 			.then(response => response.json())
 			.then(data => {
-				this.pbs = data.data;
+				//Retirar PBs de Mega Man (devem ser usados os da MMLB) e IL
+				this.srcomPBs = data.data
+					.filter(pb => 
+						!pb.game.data.names.international.startsWith('Mega Man')
+						&& pb.run.level == null
+					)
+					.map(pb => ({
+						id: pb.run.id,
+						game: pb.game.data.names.international,
+						category: pb.category.data.name,
+						time: this.formatDuration(duration(pb.run.times.primary))
+					}));	
 			});
 		},
 		searchMMLBPbs(){
-			fetch(`https://megamanleaderboards.net/index.php?page=runner&name=Titanish`, {
+			fetch(`https://megamanleaderboards.net/api/runs.php?runner=Titanish&ex=1`, {
 				headers: new Headers({
-					'User-agent': 'Mozilla/4.0 Custom User Agent',
+					'User-agent': 'Mozilla/4.0 Custom User Agent'
 				})
 			})
-			.then(response => {
-				console.log(response);
-				return response.text();
-			})
+			.then(response => response.text())
 			.then(data => {
-				console.log(data);
+				this.mmlbPBs = JSON.parse(data.replace("\'", "\\'")).runs
+					.map(pb => ({
+						id: pb.id,
+						game: pb.game.name,
+						category: pb.category,
+						time: this.formatDuration(duration(pb.converted_time * 10))
+					}));
 			});
-			
-		}
+		},
+		formatDuration(duration) { 
+			return `${duration.hours()}h ${this.padWithZero(duration.minutes())}min ${this.padWithZero(duration.seconds())}s`
+		},
+		padWithZero: number => (number+'').padStart(2, '0')
 	},
 	mounted(){
-		this.searchPbs();
+		this.searchSrcomPbs();
 		this.searchMMLBPbs();
-	}
+	},	
 }
 </script>
 
